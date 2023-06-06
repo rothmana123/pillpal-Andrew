d985ca88e2e12a9fc1e824fa10d35732d0491fd7
rothmana123 <asrothman1@usfca.edu>
2023-06-06 06:41:31 -0700

    OAuth functioning, Medication index view co-authored by ST, LM, AK

--- pillpal/main_app/models.py
+++ pillpal/main_app/models.py
@@ -15,7 +15,7 @@ class Medication(models.Model):
     dose = models.CharField(max_length=150)
     frequency = models.PositiveBigIntegerField()
     warnings = models.TextField(max_length=300)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}({self.id})'
